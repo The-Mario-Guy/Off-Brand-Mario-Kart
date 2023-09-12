db062ffa94134aa5c28cd10b75def7444412bbfd
The-Mario-Guy <111007100+The-Mario-Guy@users.noreply.github.com>
2023-09-12 12:02:20 -0700

    E

--- PSX Mario Kart/Assets/Scripts/Camera.cs
+++ PSX Mario Kart/Assets/Scripts/Camera.cs
@@ -1,22 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Camera : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject Player;
     public Vector3 offset = new Vector3(0, 1.5f, -3.3f);
+    //public float speedMod = 10.0f;
+   // private Vector3 point;
     void Start()
     {
         //Forces the Player to go at 25 FPS for the true PSX experience
         Application.targetFrameRate = 25;
+       // point = Player.transform.position;
+        //transform.LookAt(point);
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
         //Tracks the Player
-        transform.position = Player.transform.position + offset;
+         transform.position = Player.transform.position + offset;
+        //transform.RotateAround(object.transform.position, Vector3.up, speedMod * Time.deltaTime);
     }
 }
